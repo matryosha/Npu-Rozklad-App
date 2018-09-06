0adfd2b5c5832618718bf3475b5ef926ab711bf6
matryoshka <nazardjali@gmail.com>
2018-09-06 21:12:41 +0300

    init

---
+++ RozkladNpuBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RozkladNpuBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
