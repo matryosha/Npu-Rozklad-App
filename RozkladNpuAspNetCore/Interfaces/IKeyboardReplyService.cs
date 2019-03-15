f1ba2c93b8494f3ec3b9b03a380105d88d48641d
matryoshka <nazardjali@gmail.com>
2019-03-16 00:54:50 +0200

    Add IKeyboardReplyService

---
+++ RozkladNpuAspNetCore/Interfaces/IKeyboardReplyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace RozkladNpuAspNetCore.Interfaces
+{
+    public interface IKeyboardReplyService
+    {
+        Task ShowMainMenu(Message message);
+        Task ShowScheduleMenu(Message message);
+    }
+}
---
+++ RozkladNpuAspNetCore/Services/KeyboardReplyService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RozkladNpuAspNetCore.Helpers;
+using RozkladNpuAspNetCore.Interfaces;
+using Telegram.Bot.Types;
+
+namespace RozkladNpuAspNetCore.Services
+{
+    public class KeyboardReplyService : IKeyboardReplyService
+    {
+        private readonly BotService _botService;
+
+        public KeyboardReplyService(
+            BotService botService)
+        {
+            _botService = botService;
+        }
+
+        public Task ShowMainMenu(Message message)
+        {
+            return _botService.Client.SendTextMessageAsync(message.Chat.Id, "Choose action: ",
+                replyMarkup: MessageHandleHelper.GetMainMenuReplyKeyboardMarkup());
+        }
+
+        public Task ShowScheduleMenu(Message message)
+        {
+            return _botService.Client.SendTextMessageAsync(message.Chat.Id, "Groups: ",
+                replyMarkup: MessageHandleHelper.GetMainMenuReplyKeyboardMarkup());
+        }
+    }
+}
