c7cceb2a876f4f8a1e06a908f4ee7951e9b42832
matryoshka <nazardjali@gmail.com>
2019-03-16 00:48:38 +0200

    Add IUserService

---
+++ RozkladNpuAspNetCore/Interfaces/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RozkladNpuAspNetCore.Entities;
+
+namespace RozkladNpuAspNetCore.Interfaces
+{
+    public interface IUserService
+    {
+        Task AddUser(RozkladUser user);
+
+        Task UpdateUser(RozkladUser user);
+
+        Task<RozkladUser.LastActionType> GetUserLastAction(string guid);
+
+        Task<RozkladUser> GetUser(int telegramId);
+
+    }
+}
---
+++ RozkladNpuAspNetCore/Services/DatabaseOnlyUserService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RozkladNpuAspNetCore.Entities;
+using RozkladNpuAspNetCore.Interfaces;
+using RozkladNpuAspNetCore.Persistence;
+
+namespace RozkladNpuAspNetCore.Services
+{
+    public class DatabaseOnlyUserService : IUserService
+    {
+        private readonly RozkladBotContext _context;
+
+        public DatabaseOnlyUserService(
+            RozkladBotContext context)
+        {
+            _context = context;
+        }
+        public Task AddUser(RozkladUser user) => _context.Users.AddAsync(user);
+
+        public void UpdateUser(RozkladUser user) => _context.Users.Update(user);
+
+        public Task<RozkladUser> GetUser(int telegramId) => _context.Users.FirstOrDefaultAsync(u => u.TelegramId == telegramId);
+    }
+}
