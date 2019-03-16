33dc97ad86dea8820097c7d5abcd2a01ade4761a
matryoshka <nazardjali@gmail.com>
2019-03-16 17:48:52 +0200

    Add ICallbackQueryHandlerService

---
+++ RozkladNpuAspNetCore/Interfaces/ICallbackQueryHandlerService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace RozkladNpuAspNetCore.Interfaces
+{
+    public interface ICallbackQueryHandlerService
+    {
+        Task Handle(CallbackQuery callbackQuery);
+    }
+}
---
+++ RozkladNpuAspNetCore/Services/CallbackQueryHandlerService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RozkladNpuAspNetCore.Infrastructure;
+using RozkladNpuAspNetCore.Interfaces;
+using Telegram.Bot.Types;
+
+namespace RozkladNpuAspNetCore.Services
+{
+    public class CallbackQueryHandlerService : ICallbackQueryHandlerService
+    {
+        public Task Handle(CallbackQuery callbackQuery)
+        {
+            if(string.IsNullOrEmpty(callbackQuery.Data)) return Task.CompletedTask;
+            var callbackQueryData = CallbackQueryDataConverter.ConvertDataFromString(callbackQuery.Data);
+            switch (callbackQueryData.Key)
+            {
+                case RozkladCallbackQueryType.AddGroup:
+                {
+
+                    break;
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
