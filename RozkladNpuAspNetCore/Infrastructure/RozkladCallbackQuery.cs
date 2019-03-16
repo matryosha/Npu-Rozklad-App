861ca720e44b61a377310fd8333ed8cdfdf15a1e
matryoshka <nazardjali@gmail.com>
2019-03-16 17:48:09 +0200

    Add RozkladCallbackQuery

---
+++ RozkladNpuAspNetCore/Infrastructure/RozkladCallbackQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RozkladNpuAspNetCore.Infrastructure
+{
+    public enum RozkladCallbackQueryType
+    {
+        AddGroup
+    }
+
+    public class RozkladCallbackQuery
+    {
+        public RozkladCallbackQueryType UpdateType;
+    }
+}
