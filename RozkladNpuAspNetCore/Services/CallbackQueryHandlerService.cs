fa9826e0e372869a588a5a9d84d6d98df413700a
matryoshka <nazardjali@gmail.com>
2019-03-19 13:54:31 +0200

    Update CallbackQueryHandlerService
    Added handling of AddGroup and ShowGroupMenu actions

--- RozkladNpuAspNetCore/Services/CallbackQueryHandlerService.cs
+++ RozkladNpuAspNetCore/Services/CallbackQueryHandlerService.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using NpuTimetableParser;
+using RozkladNpuAspNetCore.Entities;
 using RozkladNpuAspNetCore.Infrastructure;
 using RozkladNpuAspNetCore.Interfaces;
 using Telegram.Bot.Types;
@@ -10,19 +10,49 @@ namespace RozkladNpuAspNetCore.Services
 {
     public class CallbackQueryHandlerService : ICallbackQueryHandlerService
     {
-        public Task Handle(CallbackQuery callbackQuery)
+        private readonly IKeyboardReplyService _keyboardReplyService;
+        private readonly IInlineKeyboardReplyService _inlineKeyboardReplyService;
+        private readonly IUserService _userService;
+        private readonly BotService _botService;
+
+        public CallbackQueryHandlerService(
+            IKeyboardReplyService keyboardReplyService,
+            IInlineKeyboardReplyService inlineKeyboardReplyService,
+            IUserService userService,
+            BotService botService)
         {
-            if(string.IsNullOrEmpty(callbackQuery.Data)) return Task.CompletedTask;
-            var callbackQueryData = CallbackQueryDataConverter.ConvertDataFromString(callbackQuery.Data);
+            _keyboardReplyService = keyboardReplyService;
+            _inlineKeyboardReplyService = inlineKeyboardReplyService;
+            _userService = userService;
+            _botService = botService;
+        }
+
+        public async Task Handle(CallbackQuery callbackQuery)
+        {
+            if(string.IsNullOrEmpty(callbackQuery.Data)) return;    
+            var callbackQueryData = CallbackQueryDataConverters.ConvertDataFromString(callbackQuery.Data);
+            await _botService.Client.AnswerCallbackQueryAsync(callbackQuery.Id);
             switch (callbackQueryData.Key)
             {
-                case RozkladCallbackQueryType.AddGroup:
+                case CallbackQueryType.AddGroup:
                 {
-
+                    var user = await _userService.GetUser(callbackQuery.From.Id).ConfigureAwait(false);
+                    user.LastAction = RozkladUser.LastActionType.WaitForFaculty;
+                    await _userService.UpdateUser(user);
+                    await _keyboardReplyService.ShowFacultyList(callbackQuery.Message);
+                    break;
+                }
+                case CallbackQueryType.ShowGroupMenu:
+                {
+                    await _inlineKeyboardReplyService.ShowGroupMenu(
+                        callbackQuery.Message,
+                        CallbackQueryDataConverters.ToGroup(callbackQueryData.Value),
+                        (DayOfWeek)int.Parse(callbackQueryData.Value[3]),
+                        (ShowGroupSelectedWeek)int.Parse(callbackQueryData.Value[4]));
                     break;
                 }
             }
-            return Task.CompletedTask;
+
         }
     }
 }
