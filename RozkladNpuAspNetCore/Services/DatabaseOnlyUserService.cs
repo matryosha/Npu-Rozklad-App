4896ffb99baa8f330b352eb2ee0631158c397ce5
matryoshka <nazardjali@gmail.com>
2019-03-19 17:21:32 +0200

    Simplify DatabaseOnlyUserService
    No more DbUpdateConcurrencyException

--- RozkladNpuAspNetCore/Services/DatabaseOnlyUserService.cs
+++ RozkladNpuAspNetCore/Services/DatabaseOnlyUserService.cs
@@ -42,50 +42,8 @@ namespace RozkladNpuAspNetCore.Services
                 var dbContext =
                     scope.ServiceProvider.GetRequiredService<RozkladBotContext>();
                 dbContext.Users.Update(user);
-                var saved = false;
-                while (!saved)
-                {
-                    try
-                    {
-                        await dbContext.SaveChangesAsync()
-                            .ConfigureAwait(false);
-                        saved = true;
-                    }
-                    catch (DbUpdateConcurrencyException e)
-                    {
-                        foreach (var entry in e.Entries)
-                        {
-                            if (entry.Entity is Group)
-                            {
-                                var proposedValues = entry.CurrentValues;
-                                var databaseValues = entry.GetDatabaseValues();
-
-                                if (databaseValues == null)
-                                {
-                                    entry.Da.SetValues(proposedValues);
-                                    break;
-                                }
-                                foreach (var property in proposedValues.Properties)
-                                {
-                                    var proposedValue = proposedValues[property];
-                                    var databaseValue = databaseValues[property];
-
-                                    // TODO: decide which value should be written to database
-                                    // proposedValues[property] = <value to be saved>;
-                                }
-
-                                // Refresh original values to bypass next concurrency check
-                                entry.OriginalValues.SetValues(databaseValues);
-                            }
-                            else
-                            {
-                                throw new NotSupportedException(
-                                    "Don't know how to handle concurrency conflicts for "
-                                    + entry.Metadata.Name);
-                            }
-                        }
-                    }
-                }
+                await dbContext.SaveChangesAsync()
+                    .ConfigureAwait(false);
             }
         }
 
@@ -96,9 +54,7 @@ namespace RozkladNpuAspNetCore.Services
             {
                 var dbContext =
                     scope.ServiceProvider.GetRequiredService<RozkladBotContext>();
-                user =  await dbContext.Users.Where(u => u.TelegramId == telegramId)
-                    .Include(u => u.Groups)
-                    .FirstOrDefaultAsync();
+                user = await dbContext.Users.FirstOrDefaultAsync(u => u.TelegramId == telegramId);
             }
 
             return user;
