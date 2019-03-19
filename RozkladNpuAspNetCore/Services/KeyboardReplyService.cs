e01568534812208adfd777e4afb406b83473da0f
matryoshka <nazardjali@gmail.com>
2019-03-19 13:52:04 +0200

    Update KeyboardReplyService
    Added implementations of ShowFacultyList and ShowGroupList

--- RozkladNpuAspNetCore/Services/KeyboardReplyService.cs
+++ RozkladNpuAspNetCore/Services/KeyboardReplyService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NpuTimetableParser;
 using RozkladNpuAspNetCore.Helpers;
 using RozkladNpuAspNetCore.Interfaces;
 using Telegram.Bot.Types;
@@ -11,11 +12,14 @@ namespace RozkladNpuAspNetCore.Services
     public class KeyboardReplyService : IKeyboardReplyService
     {
         private readonly BotService _botService;
+        private readonly ILessonsProvider _lessonsProvider;
 
         public KeyboardReplyService(
-            BotService botService)
+            BotService botService,
+            ILessonsProvider lessonsProvider)
         {
             _botService = botService;
+            _lessonsProvider = lessonsProvider;
         }
 
         public Task ShowMainMenu(Message message)
@@ -28,6 +32,28 @@ namespace RozkladNpuAspNetCore.Services
         {
             return _botService.Client.SendTextMessageAsync(message.Chat.Id, "Groups: ",
                 replyMarkup: MessageHandleHelper.GetMainMenuReplyKeyboardMarkup());
+        }
+
+        public Task ShowFacultyList(Message message)
+        {
+            return _botService.Client.SendTextMessageAsync(message.Chat.Id, "Choose faculty: ",
+                    replyMarkup: MessageHandleHelper.GetFacultiesReplyKeyboardMarkup(_lessonsProvider.GetFaculties()));
+        }
+
+        public async Task<bool> ShowGroupList(Message message, Faculty faculty)
+        {
+            var groupsKeyboard =  MessageHandleHelper.GetGroupsReplyKeyboardMarkup(
+                await _lessonsProvider.GetGroups(faculty.ShortName));
+
+            if (!groupsKeyboard.Keyboard.Any())
+            {
+                await _botService.Client.SendTextMessageAsync(message.Chat.Id, "There are no groups for this faculty");
+                return false;
+            }
+            
+            await _botService.Client.SendTextMessageAsync(message.Chat.Id, "Choose group: ",
+                replyMarkup: groupsKeyboard);
+            return true;
         }
     }
 }
