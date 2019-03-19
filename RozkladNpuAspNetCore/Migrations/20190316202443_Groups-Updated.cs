5d7c15262aef905fbc1d120577ade538ef3c1636
matryoshka <nazardjali@gmail.com>
2019-03-19 13:43:31 +0200

    Add migration
    Now RozkladUser contains list of group he added

---
+++ RozkladNpuAspNetCore/Migrations/20190316202443_Groups-Updated.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RozkladNpuAspNetCore.Migrations
+{
+    public partial class GroupsUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FacultyShortName",
+                table: "Group",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FacultyShortName",
+                table: "Group");
+        }
+    }
+}
