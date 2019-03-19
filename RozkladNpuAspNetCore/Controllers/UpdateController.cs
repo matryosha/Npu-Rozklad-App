62afb1e65e279082cf77169c55a17e457c6e8f9e
matryoshka <nazardjali@gmail.com>
2019-03-19 13:45:58 +0200

    Update UpdateController
    Now it also takes callback query messages

--- RozkladNpuAspNetCore/Controllers/UpdateController.cs
+++ RozkladNpuAspNetCore/Controllers/UpdateController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RozkladNpuAspNetCore.Interfaces;
 using RozkladNpuAspNetCore.Services;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -13,45 +14,67 @@ namespace RozkladNpuAspNetCore.Controllers
     public class UpdateController : ControllerBase
     {
         private readonly BotService _botService;
-        private readonly MessageHandleService _messageHandleServices;
+        private readonly IMessageHandleService _messageHandleServices;
+        private readonly ICallbackQueryHandlerService _callbackQueryHandlerService;
         private readonly ILogger _logger;
-        public UpdateController(BotService service, 
-            MessageHandleService messageHandleService,
+        public UpdateController(BotService service,
+            IMessageHandleService messageHandleService,
+            ICallbackQueryHandlerService callbackQueryHandlerService,
             ILogger<UpdateController> logger)
         {
             _botService = service;
             _messageHandleServices = messageHandleService;
+            _callbackQueryHandlerService = callbackQueryHandlerService;
             _logger = logger;
         }
         [HttpPost]
         public async Task<IActionResult> Update([FromBody] Update update)
         {
-            if (update.Type != UpdateType.Message)
+            switch (update.Type)
             {
-                return Ok();
-            }
-
-            var message = update.Message;
-
-            _logger.LogInformation("Received Message from {0}", message.Chat.Id);
-
-            if (message.Type == MessageType.Text)
-            {
-                try
+                case UpdateType.Message:
                 {
-                    await _messageHandleServices.HandleTextMessage(message);
+                    var message = update.Message;
+
+                    _logger.LogInformation("Received Message from {0}", message.Chat.Id);
+
+                    if (message.Type == MessageType.Text)
+                    {
+                        try
+                        {
+                            await _messageHandleServices.HandleMessage(message);
+                        }
+                        catch (Exception e)
+                        {
+                            _logger.LogError(4000, e, "Something wrong with message handle service");
+                            await _botService.SendErrorMessage(message.Chat.Id);
+                        }
+                    }
+                    else if (message.Type == MessageType.Sticker)
+                    {
+                        await _botService.Client.SendTextMessageAsync(message.Chat.Id, message.Sticker.FileId);
+                    }
+
+                    break;
                 }
-                catch (Exception e)
+                case UpdateType.CallbackQuery:
                 {
-                    _logger.LogError(4000, e, "Something wrong with message handle service");
-                    await _botService.SendErrorMessage(message.Chat.Id);
-                    return Ok();
+                    var callbackQuery = update.CallbackQuery;
+
+                    _logger.LogInformation("Received CallbackQuery from {0}", callbackQuery.Message.Chat.Id);
+                    try
+                    {
+                        await _callbackQueryHandlerService.Handle(callbackQuery);
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(5000, e, "Something wrong with message handle service");
+                        await _botService.SendErrorMessage(callbackQuery.Message.Chat.Id);
+                    }
+                    break;
                 }
             }
-            else if (message.Type == MessageType.Sticker)
-            {
-                await _botService.Client.SendTextMessageAsync(message.Chat.Id, message.Sticker.FileId);
-            }
+
             return Ok();
         }
     }
