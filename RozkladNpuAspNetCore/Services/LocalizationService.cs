166ceb5b779cd775017310752f4a5f96f41afab8
matryoshka <nazardjali@gmail.com>
2019-03-26 00:40:39 +0200

    Update LocalizationService
    Now returns corresponding localization value loaded from jsons

--- RozkladNpuAspNetCore/Services/LocalizationService.cs
+++ RozkladNpuAspNetCore/Services/LocalizationService.cs
@@ -1,14 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+using RozkladNpuAspNetCore.Exceptions;
+using RozkladNpuAspNetCore.Infrastructure.Localization;
 using RozkladNpuAspNetCore.Interfaces;
 
 namespace RozkladNpuAspNetCore.Services
 {
     public class LocalizationService : ILocalizationService
     {
-        public string this[string language, string text]
+        private readonly List<RozkladLocalization> _localizations;
+        public LocalizationService(
+            ILogger<LocalizationService> logger)
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            _localizations = LocalizationLoader.LoadAll();
+
+            foreach (var rozkladLocalization in _localizations)
+            {
+                try
+                {
+                    RozkladLocalizationValidator.Validate(rozkladLocalization);
+                }
+                catch (RozkladLocalizationValidationException e)
+                {
+                    logger.LogWarning("Localization validation exception", e);
+                }
+            }
+            
+        }
+
+        public LocalizationValue this[string language, string text]
+        {
+            get
+            {
+                var localization = _localizations.FirstOrDefault(l => l.ShortName == language);
+                if (localization == null)
+                    throw new Exception($"Localization for {language} is not defined");
+                if (!localization.Values.TryGetValue(text, out string value))
+                    return new LocalizationValue(string.Empty);
+
+                return new LocalizationValue(value);
+            }
         }
     }
 }
