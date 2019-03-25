fe8474f5cb113fee7057637b648ec473400bc693
matryoshka <nazardjali@gmail.com>
2019-03-25 22:03:35 +0200

    Add LocalizationValueExtensions

---
+++ RozkladNpuAspNetCore/Extensions/LocalizationValueExtensions.cs
@@ -0,0 +1,12 @@
+﻿using RozkladNpuAspNetCore.Infrastructure.Localization;
+
+namespace RozkladNpuAspNetCore.Extensions
+{
+    public static class LocalizationValueExtensions
+    {
+        public static string Active(this LocalizationValue value)
+        {
+            return $"▶ {value}";
+        }
+    }
+}
