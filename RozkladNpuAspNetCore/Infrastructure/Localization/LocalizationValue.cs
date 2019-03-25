079c57b6c0dc53f7cbfd2e842fce5cf801526a32
matryoshka <nazardjali@gmail.com>
2019-03-26 00:15:01 +0200

    Update LocalizationValue
    Add constructor

--- RozkladNpuAspNetCore/Infrastructure/Localization/LocalizationValue.cs
+++ RozkladNpuAspNetCore/Infrastructure/Localization/LocalizationValue.cs
@@ -3,7 +3,10 @@
     public class LocalizationValue
     {
         public string Value { get; set; }
-        
+        public LocalizationValue(string value)
+        {
+            Value = value;
+        }
         public override string ToString() => Value;
 
         public static implicit operator string(LocalizationValue input)
