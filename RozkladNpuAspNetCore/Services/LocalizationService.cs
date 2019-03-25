8b60d1e9046f2df06eaf6d7cf5a92c42c9257722
matryoshka <nazardjali@gmail.com>
2019-03-25 20:31:27 +0200

    Add LocalizationService

---
+++ RozkladNpuAspNetCore/Services/LocalizationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using RozkladNpuAspNetCore.Interfaces;
+
+namespace RozkladNpuAspNetCore.Services
+{
+    public class LocalizationService : ILocalizationService
+    {
+        public string this[string language, string text]
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+    }
+}
