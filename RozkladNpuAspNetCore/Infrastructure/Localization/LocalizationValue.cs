ad81a0bc94da3bc3d024d594741e889f3636d73f
matryoshka <nazardjali@gmail.com>
2019-03-25 22:02:55 +0200

    Add LocalizationValue

---
+++ RozkladNpuAspNetCore/Infrastructure/Localization/LocalizationValue.cs
@@ -0,0 +1,14 @@
+﻿namespace RozkladNpuAspNetCore.Infrastructure.Localization
+{
+    public class LocalizationValue
+    {
+        public string Value { get; set; }
+        
+        public override string ToString() => Value;
+
+        public static implicit operator string(LocalizationValue input)
+        {
+            return input.Value;
+        }
+    }
+}
