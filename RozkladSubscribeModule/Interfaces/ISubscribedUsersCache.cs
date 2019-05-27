ea818afbe772b4b703ef8b3afae6be6951ff14f0
matryoshka <nazardjali@gmail.com>
2019-05-27 22:59:39 +0300

    Fix ISubscribedUsersCache

--- RozkladSubscribeModule/Interfaces/ISubscribedUsersCache.cs
+++ RozkladSubscribeModule/Interfaces/ISubscribedUsersCache.cs
@@ -5,7 +5,7 @@ namespace RozkladSubscribeModule.Interfaces
 {
     internal interface ISubscribedUsersCache
     {
-        SubscribedUser AddUser();
+        void AddUser (SubscribedUser user);
         void RemoveUser(SubscribedUser user);
         List<SubscribedUser> GetUsers(SubscribedUser subscribedUser);
         bool IsUserExists(SubscribedUser subscribedUser);
