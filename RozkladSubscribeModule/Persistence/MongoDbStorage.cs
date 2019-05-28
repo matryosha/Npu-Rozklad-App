f898d032e2d314b0d780724a6edce7548ea351a6
matryoshka <nazardjali@gmail.com>
2019-05-29 01:18:58 +0300

    Update MongoDbStorage

--- RozkladSubscribeModule/Persistence/MongoDbStorage.cs
+++ RozkladSubscribeModule/Persistence/MongoDbStorage.cs
@@ -1,48 +1,93 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using RozkladSubscribeModule.Entities;
 using RozkladSubscribeModule.Infrastructure;
 using RozkladSubscribeModule.Interfaces;
+using RozkladSubscribeModule.Persistence.Extensions;
 
 namespace RozkladSubscribeModule.Persistence
 {
     class MongoDbStorage :
         ISubscribedUsersPersistenceStorage
     {
+        private readonly ILogger<MongoDbStorage> _logger;
         private readonly MongoClient _client;
         private readonly IMongoDatabase _database;
-        private readonly IMongoCollection<SubscribedUser> _collection;
-        public MongoDbStorage(
-            ILogger<MongoDbStorage> logger,
-            IOptions<RozkladSubscribeServiceOptions> options)
+        private readonly IMongoCollection<MongoSubscribedUser> _collection;
+
+        static MongoDbStorage()
         {
-            _client = new MongoClient(
-                options.Value.SubscribedUsersDbConnectionString);
-            _database = _client.GetDatabase("subscribed-users");
-            _collection = _database.GetCollection<SubscribedUser>("users");
+            BsonClassMap.RegisterClassMap<MongoSubscribedUser>(msu =>
+                {
+                    msu.AutoMap();
+                    msu.MapIdField(f => f.Id);
+                }
+            );
         }
 
-        public Task AddUserAsync(SubscribedUser subscribedUser)
+        public MongoDbStorage(
+            ILogger<MongoDbStorage> logger,
+            IOptions<RozkladSubscribeServiceOptions> options,
+            string databaseName = "subscribed-users",
+            string collectionName = "users",
+            bool dropDbBeforeUse = false)
         {
-            return _collection.InsertOneAsync(subscribedUser);
+            _logger = logger;
+            _client = new MongoClient(
+                options.Value.SubscribedUsersDbConnectionString);
+            if (dropDbBeforeUse)
+                _client.DropDatabase(databaseName);
+            _database = _client.GetDatabase(databaseName);
+            
+            _collection = _database.GetCollection<MongoSubscribedUser>(collectionName);
+        }
+
+        public async Task AddUserAsync(SubscribedUser subscribedUser)
+        {
+            try
+            {
+                await _collection.InsertOneAsync(subscribedUser.ToMongoSubscribedUser())
+                    .ConfigureAwait(false);
+            }
+            catch (MongoWriteException e)
+            {
+                var innerExceptionMessage = e.InnerException.Message;
+                if (Regex.Match(innerExceptionMessage, "E11000").Success)
+                    _logger.LogWarning("Adding subscribed user that already present in database");
+                else
+                    throw;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Exception when trying add subscribed user");
+            }
         }
 
         public Task DeleteUserAsync(SubscribedUser subscribedUser)
         {
-            return _collection.DeleteOneAsync(user => user == subscribedUser);
+            return _collection.DeleteOneAsync(user => user.Id == subscribedUser.GetHashCode());
         }
 
         public async Task<ICollection<SubscribedUser>> GetUsersAsync()
         {
-            return await _collection.Find(u => true).ToListAsync();
+            var mongoUsers = await _collection.Find(u => true).ToListAsync();
+            return mongoUsers.ToSubscribedUsers();
         }
 
         public Task<bool> IsUserExistsAsync(SubscribedUser subscribedUser)
         {
-            return Task.FromResult(_collection.Find(u => u == subscribedUser).Any());
+            return Task.FromResult(_collection.Find(u => u.Id == subscribedUser.GetHashCode()).Any());
+        }
+
+        public Task DropDatabase(string databaseName)
+        {
+            return _client.DropDatabaseAsync(databaseName);
         }
     }
 }
