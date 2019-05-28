48da3c742e434be9628745ea5a5dbba9e4d296ab
matryoshka <nazardjali@gmail.com>
2019-05-28 12:12:09 +0300

    Update ISubscribedUsersCache

--- RozkladSubscribeModule/Interfaces/ISubscribedUsersCache.cs
+++ RozkladSubscribeModule/Interfaces/ISubscribedUsersCache.cs
@@ -7,7 +7,8 @@ namespace RozkladSubscribeModule.Interfaces
     {
         void AddUser (SubscribedUser user);
         void RemoveUser(SubscribedUser user);
-        List<SubscribedUser> GetUsers(SubscribedUser subscribedUser);
+        List<SubscribedUser> GetUsers();
+        void SetUsers(List<SubscribedUser> users);
         bool IsUserExists(SubscribedUser subscribedUser);
     } 
 }
