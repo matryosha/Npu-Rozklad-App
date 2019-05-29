b8e8f99219e9fe5368c9ae92534efe51072ef1ef
matryoshka <nazardjali@gmail.com>
2019-05-29 17:29:35 +0300

    Add CheckTimeType enum

---
+++ RozkladSubscribeModule/Entities/CheckTimeType.cs
@@ -0,0 +1,13 @@
+﻿namespace RozkladSubscribeModule.Entities
+{
+    /// <summary>
+    /// Using for selecting timespan to check difference of schedule update
+    /// </summary>
+    internal enum CheckTimeType
+    {
+        LastDaysOfCurrentWeek,
+        LastDaysOfCurrentWeekAndNextWeek,
+        OnlyNextDay,
+        OnlyTwoNextDays
+    }
+}
