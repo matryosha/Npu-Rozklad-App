b9d0d12562d72f636115fcab80fa687fb1a771f3
matryoshka <nazardjali@gmail.com>
2019-05-29 13:48:01 +0300

    Add fields to SectionLessons

--- RozkladSubscribeModule/Entities/SectionLessons.cs
+++ RozkladSubscribeModule/Entities/SectionLessons.cs
@@ -2,6 +2,13 @@
 {
     internal class SectionLessons
     {
-        
+        public SectionLessons(string facultyShortName, int groupExternalId)
+        {
+            FacultyShortName = facultyShortName;
+            GroupExternalId = groupExternalId;
+        }
+
+        public string FacultyShortName { get; }
+        public int GroupExternalId { get; }
     }
 }
