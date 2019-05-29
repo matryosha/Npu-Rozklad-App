b1af803ee6fe0902e110c7ee3e8cf9cbdc16d967
matryoshka <nazardjali@gmail.com>
2019-05-29 17:33:43 +0300

    Add simple implementation to DefaultCheckPayload

--- RozkladSubscribeModule/Entities/DefaultCheckPayload.cs
+++ RozkladSubscribeModule/Entities/DefaultCheckPayload.cs
@@ -1,12 +1,22 @@
-﻿using RozkladSubscribeModule.Interfaces;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RozkladSubscribeModule.Interfaces;
 
 namespace RozkladSubscribeModule.Entities
 {
     internal class DefaultCheckPayload : ICheckPayload
     {
-        public bool IsDiff()
+        private HashSet<DateTime> _dateWithNewSchedule;
+        public DefaultCheckPayload()
         {
-            return false;
+            _dateWithNewSchedule = new HashSet<DateTime>();
+        }
+        public bool IsDiff() => _dateWithNewSchedule.Any();
+
+        public void AddDateWithNewSchedule(DateTime dateTime)
+        {
+            _dateWithNewSchedule.Add(dateTime);
         }
     }
 }
