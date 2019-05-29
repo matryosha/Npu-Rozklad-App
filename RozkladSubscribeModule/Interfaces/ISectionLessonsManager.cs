3b4887c20af8210dcdb95568e32338c85108eb1a
matryoshka <nazardjali@gmail.com>
2019-05-29 13:42:57 +0300

    Add ISectionLessonsManager

---
+++ RozkladSubscribeModule/Interfaces/ISectionLessonsManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using RozkladSubscribeModule.Entities;
+
+namespace RozkladSubscribeModule.Interfaces
+{
+    internal interface ISectionLessonsManager
+    {
+        Task<SectionLessons> GetCurrentSectionLessons(
+            int groupExternalId,
+            string facultyShortName);
+
+        SectionLessons GetLastSectionLessons(
+            int groupExternalId,
+            string facultyShortName);
+
+        void UpdateLastSection(
+            SectionLessons sectionLessons,
+            int groupExternalId,
+            string facultyShortName);
+    }
+}
