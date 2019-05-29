e6a23c2fff25e8a2f5f8348e413f1cc1e3a39641
matryoshka <nazardjali@gmail.com>
2019-05-29 17:35:11 +0300

    Update SectionLessons

--- RozkladSubscribeModule/Entities/SectionLessons.cs
+++ RozkladSubscribeModule/Entities/SectionLessons.cs
@@ -1,4 +1,8 @@
-﻿namespace RozkladSubscribeModule.Entities
+﻿using System;
+using System.Collections.Generic;
+using NpuTimetableParser;
+
+namespace RozkladSubscribeModule.Entities
 {
     internal class SectionLessons
     {
@@ -6,9 +10,23 @@
         {
             FacultyShortName = facultyShortName;
             GroupExternalId = groupExternalId;
+            Lessons = new Dictionary<DateTime, List<Lesson>>();
+            Dates = new List<DateTime>();
+        }
+
+        public List<Lesson> this[DateTime date]
+        {
+            get
+            {
+                if (Lessons.TryGetValue(date, out var result))
+                    return result;
+                return new List<Lesson>();
+            }
         }
 
         public string FacultyShortName { get; }
         public int GroupExternalId { get; }
+        public List<DateTime> Dates { get; set; }
+        public Dictionary<DateTime, List<Lesson>> Lessons { get; set; }
     }
 }
