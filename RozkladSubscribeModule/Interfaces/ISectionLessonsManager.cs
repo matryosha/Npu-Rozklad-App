3a732bd04a56983bbe61455f490774e03e1cf1f5
matryoshka <nazardjali@gmail.com>
2019-05-30 21:14:27 +0300

    Add SectionLessons to GetLastSectionLessons method

--- RozkladSubscribeModule/Interfaces/ISectionLessonsManager.cs
+++ RozkladSubscribeModule/Interfaces/ISectionLessonsManager.cs
@@ -11,7 +11,8 @@ namespace RozkladSubscribeModule.Interfaces
 
         SectionLessons GetLastSectionLessons(
             int groupExternalId,
-            string facultyShortName);
+            string facultyShortName,
+            SectionLessons currentSectionLessons = null);
 
         void UpdateLastSection(
             SectionLessons sectionLessons);
