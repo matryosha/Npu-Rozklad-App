dab156306cac2e2e2af7b1a7633dc4a0051d5455
matryoshka <nazardjali@gmail.com>
2019-05-30 19:21:23 +0300

    Add DateTimesForScheduleDiffCheckGiver

--- RozkladSubscribeModule/Infrastructure/RozkladSubscribeServiceOptions.cs
+++ RozkladSubscribeModule/Infrastructure/RozkladSubscribeServiceOptions.cs
@@ -1,7 +1,11 @@
-﻿namespace RozkladSubscribeModule.Infrastructure
+﻿using RozkladSubscribeModule.Entities;
+using RozkladSubscribeModule.Infrastructure.Enums;
+
+namespace RozkladSubscribeModule.Infrastructure
 {
     public class RozkladSubscribeServiceOptions
     {
         public string SubscribedUsersDbConnectionString { get; set; }
+        public CheckTimeType CheckTimeType { get; set; }
     }
 }
