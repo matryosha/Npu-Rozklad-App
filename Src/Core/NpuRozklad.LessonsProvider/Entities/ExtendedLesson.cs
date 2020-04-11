500c6591ccf828ec20edf47dfb3c6c7fb19fbea6
matryoshka <nazardjali@gmail.com>
2020-04-11 17:51:36 +0300

    Add ExtendedLesson

---
+++ Src/Core/NpuRozklad.LessonsProvider/Entities/ExtendedLesson.cs
@@ -0,0 +1,17 @@
+using System;
+using NpuRozklad.Core.Entities;
+
+namespace NpuRozklad.LessonsProvider.Entities
+{
+    public class ExtendedLesson : Lesson
+    {
+        /// <summary>
+        /// Contains date when lesson has been set up. Not actual date
+        /// </summary>
+        public DateTime LessonSetUpDate { get; set; }
+        /// <summary>
+        /// Indicates how many times lesson must appear in timetable
+        /// </summary>
+        public int LessonCount { get; set; }
+    }
+}
