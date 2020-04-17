e201e4065fc9c0a562697ba89168ec37cbce864d
matryoshka <nazardjali@gmail.com>
2020-04-17 17:30:43 +0300

    Update Group.cs
    Added Faculty property

--- Src/Core/NpuRozklad.Core/Entities/Group.cs
+++ Src/Core/NpuRozklad.Core/Entities/Group.cs
@@ -5,16 +5,18 @@ namespace NpuRozklad.Core.Entities
     public class Group : TypeIdTrait
     {
         public string Name { get; }
+        public Faculty Faculty { get; }
 
         public override string ToString()
         {
             return Name;
         }
 
-        public Group(string typeId, string name) : base(typeId)
+        public Group(string typeId, string name, Faculty faculty) : base(typeId)
         {
             if (string.IsNullOrWhiteSpace(name))
                 throw new ArgumentException("Group name cannot be null or empty");
+            Faculty = faculty ?? throw new ArgumentNullException(nameof(faculty), "Faculty cannot be null");
             Name = name;
         }
     }
