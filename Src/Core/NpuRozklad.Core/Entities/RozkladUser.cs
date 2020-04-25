af0845378465ff705a76ac20d480edb83f49f5ce
matryoshka <nazardjali@gmail.com>
2020-04-25 16:28:28 +0300

    Add RozkladUser.cs

---
+++ Src/Core/NpuRozklad.Core/Entities/RozkladUser.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace NpuRozklad.Core.Entities
+{
+    public class RozkladUser
+    {
+        public RozkladUser()
+        { }
+
+        protected RozkladUser(RozkladUser origin)
+        {
+            Guid = origin.Guid;
+            IsDeleted = origin.IsDeleted;
+            FacultyGroups = origin.FacultyGroups;
+        }
+        public string Guid { get; } = System.Guid.NewGuid().ToString();
+        public List<Group> FacultyGroups { get; } = new List<Group>();
+        public bool IsDeleted { get; set; }
+    }
+}
