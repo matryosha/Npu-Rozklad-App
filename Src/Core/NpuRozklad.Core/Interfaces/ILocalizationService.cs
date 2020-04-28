5e3828b7eb83c150e73f203daeee58434f056d6d
matryoshka <nazardjali@gmail.com>
2020-04-29 01:39:41 +0300

    Add ILocalizationService.cs

---
+++ Src/Core/NpuRozklad.Core/Interfaces/ILocalizationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace NpuRozklad.Core.Interfaces
+{
+    public interface ILocalizationService
+    {
+        LocalizationValue this[string language, string text] { get; }
+        LocalizationValue this[string language, DayOfWeek dayOfWeek, bool asFullDayName = false] { get; }
+        string DefaultLanguage { get; }
+    }
+}
