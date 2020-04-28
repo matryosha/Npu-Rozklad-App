67d8c09a7aa5b3366e963eb205186367a9ffecc2
matryoshka <nazardjali@gmail.com>
2020-04-29 01:39:30 +0300

    Add LocalizationValue.cs

---
+++ Src/Core/NpuRozklad.Core/LocalizationValue.cs
@@ -0,0 +1,17 @@
+﻿namespace NpuRozklad.Core
+{
+    public class LocalizationValue
+    {
+        public string Value { get; set; }
+        public LocalizationValue(string value)
+        {
+            Value = value;
+        }
+        public override string ToString() => Value;
+
+        public static implicit operator string(LocalizationValue input)
+        {
+            return input.Value;
+        }
+    }
+}
