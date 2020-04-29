3ada24db65f2dbdc6ba61be7ae7618e665442c22
matryoshka <nazardjali@gmail.com>
2020-04-29 15:57:58 +0300

    Update IDayOfWeekToDateTimeConverter.cs
    Updated documentation

--- Src/NpuRozklad.Core/Interfaces/IDayOfWeekToDateTimeConverter.cs
+++ Src/NpuRozklad.Core/Interfaces/IDayOfWeekToDateTimeConverter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace NpuRozklad.Core.Interfaces
 {
@@ -9,15 +10,23 @@ namespace NpuRozklad.Core.Interfaces
     {
         /// <summary>
         /// Converts <see cref="System.DayOfWeek"/> to local <see cref="System.DateTime"/>
-        /// Example: If current date is April 21 and it's tuesday.
+        /// <para>Example: If current date is April 21 and it's tuesday then:</para>
         /// 
-        /// Then when <paramref name="dayOfWeek"/> is <see cref="DayOfWeek.Monday"/> and
-        /// <paramref name="asNextWeekDate"/> is false then returned value would be April 20 Monday
-        ///
+        /// <para>
+        /// When <paramref name="dayOfWeek"/> is <see cref="DayOfWeek.Monday"/> and
+        /// <paramref name="asNextWeekDate"/> is <c>false</c> then returned value would be April 20 Monday.
+        /// </para>
+        /// 
+        /// <para>
         /// When <paramref name="dayOfWeek"/> is <see cref="DayOfWeek.Tuesday"/> and
-        /// <paramref name="asNextWeekDate"/> is True then returned value would be April 28 Tuesday
+        /// <paramref name="asNextWeekDate"/> is <c>true</c> then returned value would be April 28 Tuesday.
+        /// </para>
+        ///
+        /// <para>
+        /// Note that method considers <see cref="DateTimeFormatInfo.FirstDayOfWeek"/> as <see cref="DayOfWeek.Monday"/>
+        /// </para>
         /// </summary>
-        /// <param name="dayOfWeek"></param>
+        /// <param name="dayOfWeek"><see cref="System.DayOfWeek"/> value to get the date from</param>
         /// <param name="asNextWeekDate">When true returned datetime will be for next week</param>
         /// <returns>Relative to current day <see cref="System.DateTime"/> for which <see cref="System.DayOfWeek"/>
         /// is <paramref name="dayOfWeek"/></returns>
