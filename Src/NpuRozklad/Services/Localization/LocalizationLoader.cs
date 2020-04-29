f23b134f19368cfbe4685c928d14152e545da50c
matryoshka <nazardjali@gmail.com>
2020-04-30 00:04:15 +0300

    Add ILocalizationService implementation

---
+++ Src/NpuRozklad/Services/Localization/LocalizationLoader.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace NpuRozklad.Services.Localization
+{
+    public class LocalizationLoader
+    {
+        private string _localizationFilesPath;
+
+        public LocalizationLoader(LocalizationLoaderOptions options)
+        {
+            Init(options);
+        }
+
+        public Dictionary<string, RozkladLocalization> LoadAll()
+        {
+            var localizationFilesPaths = GetPathToLocalizationFiles();
+
+            return localizationFilesPaths.Select(path =>
+                    JsonConvert.DeserializeObject<RozkladLocalization>(File.ReadAllText(path)))
+                .ToDictionary(localizationValue => localizationValue.ShortName);
+        }
+
+        private void Init(LocalizationLoaderOptions options)
+        {
+            if (string.IsNullOrWhiteSpace(_localizationFilesPath))
+                throw new ArgumentNullException(nameof(options.PathToLocalizationsFiles),
+                    "Path to localization files is empty");
+
+            CheckLocalizationPath(options.PathToLocalizationsFiles);
+        }
+
+        private void CheckLocalizationPath(string optionPath)
+        {
+            _localizationFilesPath = Path.Combine(Directory.GetCurrentDirectory(), optionPath);
+            if (!Directory.Exists(_localizationFilesPath))
+                throw new ArgumentException("Localizations directory doesn't exist");
+        }
+
+        private string[] GetPathToLocalizationFiles()
+        {
+            var filePaths = Directory.GetFiles(_localizationFilesPath);
+            if (!filePaths.Any())
+                throw new Exception("Can't find any localization file");
+            return filePaths;
+        }
+    }
+
+    public class LocalizationLoaderOptions
+    {
+        public string PathToLocalizationsFiles { get; set; }
+    }
+}
