5d09e71451d02eb9782d562aadee8ec3fd63f6ac
matryoshka <nazardjali@gmail.com>
2020-05-03 01:42:34 +0300

    Update RozkladUserWrapper.cs

--- Src/NpuRozklad.Persistence/RozkladUserWrapper.cs
+++ Src/NpuRozklad.Persistence/RozkladUserWrapper.cs
@@ -6,18 +6,17 @@ namespace NpuRozklad.Persistence
 {
     internal class RozkladUserWrapper : RozkladUser
     {
-
-        public RozkladUserWrapper()
+        public RozkladUserWrapper(string guid = null)
+            : base(guid)
         {
-            
         }
-        
+
         public RozkladUserWrapper(RozkladUser rozkladUser)
             : base(rozkladUser)
         {
             FacultyGroupsTypeIds = FacultyGroups.Select(g => g.TypeId).ToList();
         }
-        
+
         public List<string> FacultyGroupsTypeIds { get; set; }
     }
 }
