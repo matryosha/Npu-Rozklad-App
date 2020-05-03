5dc3ece89b107dfb07aca38e8681f692db045246
matryoshka <nazardjali@gmail.com>
2020-05-03 22:42:04 +0300

    Update CallbackQueryGlobalHandler.cs
    Keeping callback message id

--- Src/NpuRozklad.Telegram/Handlers/CallbackQueryHandlers/CallbackQueryGlobalHandler.cs
+++ Src/NpuRozklad.Telegram/Handlers/CallbackQueryHandlers/CallbackQueryGlobalHandler.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
 using NpuRozklad.Telegram.Helpers;
+using NpuRozklad.Telegram.Interfaces;
+using NpuRozklad.Telegram.Services.Interfaces;
 using Telegram.Bot.Types;
 
 namespace NpuRozklad.Telegram.Handlers.CallbackQueryHandlers
@@ -7,15 +9,20 @@ namespace NpuRozklad.Telegram.Handlers.CallbackQueryHandlers
     internal class CallbackQueryGlobalHandler : ICallbackQueryHandler
     {
         private readonly SpecificCallbackQueryHandlerProvider _queryHandlerProvider;
+        private readonly IExternalServiceProvider _externalServiceProvider;
 
-        public CallbackQueryGlobalHandler(SpecificCallbackQueryHandlerProvider queryHandlerProvider)
+        public CallbackQueryGlobalHandler(SpecificCallbackQueryHandlerProvider queryHandlerProvider,
+            IExternalServiceProvider externalServiceProvider)
         {
             _queryHandlerProvider = queryHandlerProvider;
+            _externalServiceProvider = externalServiceProvider;
         }
         
         public Task Handle(CallbackQuery callbackQuery)
         {
-            /* 
+            /*
+             * Get message id from callback and store it
+             * 
              * Callback data to KeyValue pairs
              *               \/
              *               V
@@ -23,10 +30,16 @@ namespace NpuRozklad.Telegram.Handlers.CallbackQueryHandlers
              * Delegate handling to specific handler
              * 
              */
-
+            StoreMessageId(callbackQuery);
             var callbackQueryData = CallbackDataFormatter.DeserializeCallbackQueryData(callbackQuery.Data);
             var handler = _queryHandlerProvider.GetHandler(callbackQueryData.CallbackQueryActionType);
             return handler.Handle(callbackQueryData);
+        }
+
+        private void StoreMessageId(CallbackQuery callbackQuery)
+        {
+            var scopeMessageIdProvider = _externalServiceProvider.GetService<ICurrentScopeMessageIdProvider>();
+            scopeMessageIdProvider.MessageId = callbackQuery.Message?.MessageId;
         }
     }
 }
