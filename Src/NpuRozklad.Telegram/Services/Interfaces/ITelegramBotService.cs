99f58f85e94ba1bde81c47ca6d86ca313ecc126a
matryoshka <nazardjali@gmail.com>
2020-05-03 22:42:25 +0300

    Update ITelegramBotService.cs
    Added SendOrEditMessageAsync method

--- Src/NpuRozklad.Telegram/Services/Interfaces/ITelegramBotService.cs
+++ Src/NpuRozklad.Telegram/Services/Interfaces/ITelegramBotService.cs
@@ -1,9 +1,26 @@
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace NpuRozklad.Telegram.Services.Interfaces
 {
     public interface ITelegramBotService
     {
         ITelegramBotClient Client { get; }
+
+        Task<Message> SendOrEditMessageAsync(
+            ChatId chatId,
+            string text,
+            ParseMode parseMode = ParseMode.Default,
+            bool disableWebPagePreview = false,
+            bool disableNotification = false,
+            int replyToMessageId = 0,
+            IReplyMarkup replyMarkup = null,
+            CancellationToken cancellationToken = default,
+            bool forceNewMessage = false
+        );
     }
 }
