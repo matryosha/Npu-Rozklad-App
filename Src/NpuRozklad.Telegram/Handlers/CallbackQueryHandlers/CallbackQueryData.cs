40caf499826af82c9aaa6a55c74846851b1fec8c
matryoshka <nazardjali@gmail.com>
2020-05-04 19:30:04 +0300

    Update CallbackQueryData.cs
    Added CallbackQueryId property

--- Src/NpuRozklad.Telegram/Handlers/CallbackQueryHandlers/CallbackQueryData.cs
+++ Src/NpuRozklad.Telegram/Handlers/CallbackQueryHandlers/CallbackQueryData.cs
@@ -2,6 +2,7 @@ namespace NpuRozklad.Telegram.Handlers.CallbackQueryHandlers
 {
     public class CallbackQueryData
     {
+        public string CallbackQueryId { get; set; }
         public CallbackQueryActionType CallbackQueryActionType { get; set; }
         public string[] Values { get; set; }
     }
