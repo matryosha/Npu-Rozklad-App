d74a5cd68a1f45755553f84847621580c123fc15
matryoshka <nazardjali@gmail.com>
2020-05-04 21:47:42 +0300

    Update ShowFacultyGroupsForFacultyDoesNotExistMessageAction.cs
    Refactored

--- Src/NpuRozklad.Telegram/BotActions/ShowFacultyGroupsForFacultyDoesNotExistMessageAction.cs
+++ Src/NpuRozklad.Telegram/BotActions/ShowFacultyGroupsForFacultyDoesNotExistMessageAction.cs
@@ -1,5 +1,4 @@
 using System.Threading.Tasks;
-using NpuRozklad.Core.Interfaces;
 using NpuRozklad.Telegram.Display.Common.Controls;
 using NpuRozklad.Telegram.Services.Interfaces;
 
@@ -8,24 +7,21 @@ namespace NpuRozklad.Telegram.BotActions
     public class ShowFacultyGroupsForFacultyDoesNotExistMessageAction
     {
         private readonly ITelegramBotService _telegramBotService;
-        private readonly ILocalizationService _localizationService;
-        private readonly ICurrentTelegramUserService _currentUserService;
+        private readonly ICurrentUserLocalizationService _currentUserLocalizationService;
         private readonly MainMenuCreator _mainMenuCreator;
 
         public ShowFacultyGroupsForFacultyDoesNotExistMessageAction(ITelegramBotService telegramBotService,
-            ILocalizationService localizationService,
-            ICurrentTelegramUserService currentUserService, 
+            ICurrentUserLocalizationService currentUserLocalizationService,
             MainMenuCreator mainMenuCreator)
         {
             _telegramBotService = telegramBotService;
-            _localizationService = localizationService;
-            _currentUserService = currentUserService;
+            _currentUserLocalizationService = currentUserLocalizationService;
             _mainMenuCreator = mainMenuCreator;
         }
 
         public Task Execute(ShowFacultyGroupsForFacultyDoesNotExistMessageOptions options = null)
         {
-            var messageText = _localizationService[_currentUserService.Language, "no-groups-for-faculty-message"];
+            var messageText = _currentUserLocalizationService["no-groups-for-faculty-message"];
 
             var mainMenuKeyboard = _mainMenuCreator.CreateMenu();
 
