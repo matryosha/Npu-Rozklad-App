d7e9a27b0963c3e4b87f40b4605f038141d6b206
matryoshka <nazardjali@gmail.com>
2020-05-07 00:02:05 +0300

    Update UnprocessedExtendedLessonsManager.cs
    Surrounded with try finally to leave locks

--- Src/NpuRozklad.LessonsProvider/UnprocessedExtendedLessonsManager.cs
+++ Src/NpuRozklad.LessonsProvider/UnprocessedExtendedLessonsManager.cs
@@ -46,34 +46,39 @@ namespace NpuRozklad.LessonsProvider
             // if not populate cache
             // return value
             ICollection<ExtendedLesson> result;
-            
             var facultyLock = _facultyLocks.GetOrAdd(faculty, new OneManyLock());
-            facultyLock.Enter(false);
-            _cacheLock.Enter(false);
-
-            if(!_unprocessedLessonsCache.ContainsKey(faculty))
-            {
-                facultyLock.Leave();
-                _cacheLock.Leave();
-                facultyLock.Enter(true);
-                _cacheLock.Enter(false);
-                
-                if (!_unprocessedLessonsCache.ContainsKey(faculty))
-                {
-                    _cacheLock.Leave();
-                    
-                    var facultyRawLessons = await GetFacultyRawLessonsInternal(faculty);
-
-                    _cacheLock.Enter(true);
-                    _unprocessedLessonsCache.Add(faculty, facultyRawLessons);
-                }
-            }
-
-            result = new List<ExtendedLesson>(_unprocessedLessonsCache[faculty]);
-            _cacheLock.Leave();
-            facultyLock.Leave();
             
-            return result;
+            try
+            {
+                facultyLock.Enter(false);
+                _cacheLock.Enter(false);
+
+                if(!_unprocessedLessonsCache.ContainsKey(faculty))
+                {
+                    facultyLock.Leave();
+                    _cacheLock.Leave();
+                    facultyLock.Enter(true);
+                    _cacheLock.Enter(false);
+                
+                    if (!_unprocessedLessonsCache.ContainsKey(faculty))
+                    {
+                        _cacheLock.Leave();
+                    
+                        var facultyRawLessons = await GetFacultyRawLessonsInternal(faculty);
+
+                        _cacheLock.Enter(true);
+                        _unprocessedLessonsCache.Add(faculty, facultyRawLessons);
+                    }
+                }
+
+                result = new List<ExtendedLesson>(_unprocessedLessonsCache[faculty]);
+                return result;
+            }
+            finally
+            {
+                _cacheLock.Leave();
+                facultyLock.Leave();
+            }
         }
 
         private async Task<ICollection<ExtendedLesson>> GetFacultyRawLessonsInternal(Faculty faculty)
