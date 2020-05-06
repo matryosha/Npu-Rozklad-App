a94f23ae1930fe77327921ef19287bc4e8766608
matryoshka <nazardjali@gmail.com>
2020-05-06 18:25:13 +0300

    Update LessonsProviderResult.cs

--- Src/NpuRozklad.Core/Entities/LessonsProviderResult.cs
+++ Src/NpuRozklad.Core/Entities/LessonsProviderResult.cs
@@ -4,7 +4,7 @@ namespace NpuRozklad.Core.Entities
 {
     public class LessonsProviderResult
     {
-        public LessonsProviderResult(IReadOnlyCollection<Lesson> lessons, bool isNpuServerOffline, bool isErrorOccured)
+        public LessonsProviderResult(IReadOnlyCollection<Lesson> lessons, bool isNpuServerOffline = false, bool isErrorOccured = false)
         {
             Lessons = lessons;
             IsNpuServerOffline = isNpuServerOffline;
