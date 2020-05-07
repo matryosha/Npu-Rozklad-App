bef01129579f0bf20de09fd3f5fe85796ff1a24a
matryoshka <nazardjali@gmail.com>
2020-05-07 18:16:24 +0300

    Update LongLastingUserActionGeneralHandler.cs
    Added exceptions handling

--- Src/NpuRozklad.Telegram/Handlers/MessageHandlers/LongLastingUserActionGeneralHandler.cs
+++ Src/NpuRozklad.Telegram/Handlers/MessageHandlers/LongLastingUserActionGeneralHandler.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using NpuRozklad.Telegram.Exceptions;
 using NpuRozklad.Telegram.LongLastingUserActions;
 using NpuRozklad.Telegram.Services.Interfaces;
 using Telegram.Bot.Types;
@@ -10,31 +13,46 @@ namespace NpuRozklad.Telegram.Handlers.MessageHandlers
         private readonly ILongLastingUserActionManager _longLastingUserActionManager;
         private readonly ICurrentTelegramUserProvider _currentTelegramUserProvider;
         private readonly ILongLastingUserActionHandlerFactory _longLastingUserActionHandlerFactory;
+        private readonly ILogger<LongLastingUserActionGeneralHandler> _logger;
 
         public LongLastingUserActionGeneralHandler(
             ILongLastingUserActionManager longLastingUserActionManager,
             ICurrentTelegramUserProvider currentTelegramUserProvider,
-            ILongLastingUserActionHandlerFactory longLastingUserActionHandlerFactory)
+            ILongLastingUserActionHandlerFactory longLastingUserActionHandlerFactory,
+            ILogger<LongLastingUserActionGeneralHandler> logger)
         {
             _longLastingUserActionManager = longLastingUserActionManager;
             _currentTelegramUserProvider = currentTelegramUserProvider;
             _longLastingUserActionHandlerFactory = longLastingUserActionHandlerFactory;
+            _logger = logger;
         }
 
         public async Task<bool> Handle(Message message)
         {
-            bool isHandled = false;
             var currentUser = _currentTelegramUserProvider.GetCurrentTelegramRozkladUser();
-            var userLongLastingActionArguments =
-                await _longLastingUserActionManager.GetUserLongLastingAction(currentUser);
+            try
+            {
+                bool isHandled = false;
+                
+                var userLongLastingActionArguments =
+                    await _longLastingUserActionManager.GetUserLongLastingAction(currentUser);
 
-            if (userLongLastingActionArguments == null) return isHandled;
+                if (userLongLastingActionArguments == null) return isHandled;
             
-            userLongLastingActionArguments.Parameters[typeof(Message)] = message;
-            var handler = _longLastingUserActionHandlerFactory.GetHandler(userLongLastingActionArguments);
-            isHandled = await handler.Handle(userLongLastingActionArguments);
+                userLongLastingActionArguments.Parameters[typeof(Message)] = message;
+                var handler = _longLastingUserActionHandlerFactory.GetHandler(userLongLastingActionArguments);
+                isHandled = await handler.Handle(userLongLastingActionArguments);
             
-            return isHandled;
+                return isHandled;
+            }
+            catch (Exception e)
+            {
+                await _longLastingUserActionManager.ClearUserAction(currentUser);
+                var outException = new LongLastingUserActionHandlerException(currentUser.TelegramId, e);
+                _logger.LogError(outException, outException.Message);
+                
+                return false;
+            }
         }
     }
 }
