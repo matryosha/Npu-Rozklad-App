50ba57196d0b32245f259373a285981906ca9fd6
matryoshka <nazardjali@gmail.com>
2020-05-08 00:17:33 +0300

    Update NpuRozklad.Persistence
    Added init ef migration

---
+++ Src/NpuRozklad.Persistence/EfMigrations/20200507211220_Init.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NpuRozklad.Persistence.EfMigrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RozkladUserWrappers",
+                columns: table => new
+                {
+                    Guid = table.Column<string>(nullable: false),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    FacultyGroupsTypeIds = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RozkladUserWrappers", x => x.Guid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RozkladUserWrappers");
+        }
+    }
+}
