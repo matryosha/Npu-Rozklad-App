54faf6a67afa4009b15e16af99c0b70d88fdb108
matryoshka <nazardjali@gmail.com>
2020-05-08 21:26:05 +0300

    Update CallbackQueryData.cs
    Added ToString override

--- Src/NpuRozklad.Telegram/Handlers/CallbackQueryHandlers/CallbackQueryData.cs
+++ Src/NpuRozklad.Telegram/Handlers/CallbackQueryHandlers/CallbackQueryData.cs
@@ -5,5 +5,9 @@ namespace NpuRozklad.Telegram.Handlers.CallbackQueryHandlers
         public string CallbackQueryId { get; set; }
         public CallbackQueryActionType CallbackQueryActionType { get; set; }
         public string[] Values { get; set; }
+
+        public override string ToString() =>
+            $"{nameof(CallbackQueryId)}: {CallbackQueryId}. {nameof(CallbackQueryActionType)}: {CallbackQueryActionType} " +
+            $"{nameof(Values)}: {string.Join(", ", Values)}";
     }
 }
