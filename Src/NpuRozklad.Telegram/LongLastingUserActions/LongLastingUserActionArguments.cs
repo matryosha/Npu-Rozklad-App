63ed6170ce50281ddbc6a9a15eedb1e2c4c7aff2
matryoshka <nazardjali@gmail.com>
2020-05-08 21:35:55 +0300

    Update LongLastingUserActionArguments.cs
    Added ToString override

--- Src/NpuRozklad.Telegram/LongLastingUserActions/LongLastingUserActionArguments.cs
+++ Src/NpuRozklad.Telegram/LongLastingUserActions/LongLastingUserActionArguments.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NpuRozklad.Telegram.LongLastingUserActions
 {
@@ -8,5 +9,9 @@ namespace NpuRozklad.Telegram.LongLastingUserActions
         public LongLastingUserActionType UserActionType { get; set; }
         public TelegramRozkladUser TelegramRozkladUser { get; set; }
         public Dictionary<Type, object> Parameters { get; set; } = new Dictionary<Type, object>();
+
+        public override string ToString() =>
+            $"{nameof(UserActionType)}: {UserActionType}. {nameof(TelegramRozkladUser)}: {TelegramRozkladUser}. " +
+            $"{nameof(Parameters)}: {string.Join(";", Parameters.Select(x => x.Key + "=" + x.Value).ToString())}";
     }
 }
